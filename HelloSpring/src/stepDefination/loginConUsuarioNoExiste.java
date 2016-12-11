d99b9cd59a9f7cae7cf5152f58028b27a279c060
pablo <pablo@DESKTOP-87GNL7V>
2016-12-11 16:53:35 +0100

    Ultimos tests.

---
+++ HelloSpring/src/stepDefination/loginConUsuarioNoExiste.java
@@ -0,0 +1,43 @@
+package stepDefination;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class loginConUsuarioNoExiste {
+
+	WebDriver driver = new FirefoxDriver();
+
+	@Given("^Abrimos Mozilla Firefox$")
+	public void Abrimos_Mozilla_Firefox() throws Throwable {
+		System.setProperty("webdriver.gecko.driver","geckodriver.exe");
+		driver.get("http://localhost:8080/HelloSpring/");
+		Thread.sleep(1000);
+	}
+
+	@When("^Insertamos nombre y password en la barra$")
+	public void Insertamos_nombre_contraseña() throws Throwable {
+		WebElement barraNombre = driver.findElement(By.name("user"));
+		WebElement password = driver.findElement(By.name("password"));
+		barraNombre.sendKeys("asdfasdfasdfasd");
+		password.sendKeys("pabfasdfasdfasdfasdfasdflo");;
+		Thread.sleep(1000);
+	}
+	
+	@Then("^boton aceptar$")
+	public void aceptar() throws Throwable {
+		WebElement boton= driver.findElement(By.name("aceptar"));
+		boton.click();
+		Thread.sleep(1000);
+	}	
+
+	@Then("^cerraremos el navegador$")
+	public void cerraremos_el_navegador() throws Throwable {
+	    driver.quit();
+	}
+}
