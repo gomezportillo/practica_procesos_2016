2c1b4fe67e5b36052b9678e0b4799159cdb7426e
Pedro Manuel Gómez-Portillo <pedromanuel.gomezportillo@alu.uclm.es>
2016-12-19 15:40:07 +0100

    patron singleton implementado en clase emailsender

--- HelloSpring/src/com/esi/uclm/procesos/gestion/EmailSender.java
+++ HelloSpring/src/com/esi/uclm/procesos/gestion/EmailSender.java
@@ -6,36 +6,46 @@ import javax.mail.internet.*;
 
 public class EmailSender {
 
-	final String username = "pis.uclm.2016@gmail.com";
-	final String password = "pisuclm2016";   
-	String content = "Existen tareas que requieren su atención, ";
-	String to_email;
+	private static EmailSender _instance;
 
-	public EmailSender (String to_email) {    
-		this.to_email= to_email;
+	private final String username = "pis.uclm.2016@gmail.com";
+	private final String password = "pisuclm2016";
+
+	private Properties _props;
+
+	private EmailSender () {
+		_props = new Properties();
+		_props.put("mail.smtp.auth", "true");
+		_props.put("mail.smtp.starttls.enable", "true");
+		_props.put("mail.smtp.host", "smtp.gmail.com");
+		_props.put("mail.smtp.port", "587");
 	}
 
-	public void send() {
+	public static EmailSender getInstance() {
+		if (_instance == null) _instance = new EmailSender();
+		return _instance;
+	}
 
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
+	public void send(String to_email) {
 
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+		Session session = Session.getInstance(_props, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(username, password);
 			}
 		});
 
 		try {
-
 			Message message = new MimeMessage(session);
+
 			message.setFrom(new InternetAddress(username));
+
 			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to_email));
+
 			message.setSubject("TODO App - Información");
-			message.setText(content + to_email.split("@")[0]);
+
+			String msg = "Buenas " + to_email.split("@")[0] + ",\n\n";
+			msg += "existen tareas que requieren su atención. Acceda al sitio web para verlas.";		
+			message.setText(msg);
 
 			Transport.send(message);
 
